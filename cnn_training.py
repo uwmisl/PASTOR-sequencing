6f0b3247a62d01c4c3f93e712b1c978404072714
daphkh <daphkh@users.noreply.github.com>
2023-06-26 23:45:17 -0700

    data

---
+++ cnn_training.py
@@ -0,0 +1,276 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import os, time
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+import math
+import warnings
+import pandas as pd
+import scipy.signal as scisignal
+import sklearn.utils.class_weight as class_weight
+from sklearn.decomposition import PCA
+
+# load data
+segments_df = pd.read_json('data/pretty_segments_df.json')
+channels_arr = list(np.load(open('data/channels_arr.npy','rb')))
+
+# fixed held out test set runs
+chosen_runs = ['20220824_run02_a', '20220826_run02_a', '20220826_run03_a', '20220907_run01_a', '20221213_run02_a', '20221214_run01_a']
+
+# features and labels    
+feature_cols = ['median', 'max', 'middle', 'mean_abs_deriv', 'median_abs_deriv','mean','raw_std', 'dip']
+index_to_aa = [c for c in 'CSAGTVNQMILYWFPHRKDE']
+aa_to_index = {aa:i for i, aa in enumerate(index_to_aa)}
+
+# change putative deamidated N labels
+for name, row in segments_df[segments_df.aa == 'N'][segments_df['max'] > 1.3].iterrows():
+    segments_df.at[name, 'aa'] = 'D'
+    
+# stretch signals, to account for variable lenths
+def stretch(arr, new_len):
+    if len(arr) == new_len:
+        return arr
+    return torch.tensor([arr[int(i/new_len*len(arr))] for i in range(new_len)])
+
+class MyDataset(Dataset):
+    def __init__(self, input_data, output_data):
+        self.input_data = input_data
+        self.output_data = output_data
+
+    def __len__(self):
+        return len(self.input_data)
+
+    def __getitem__(self, idx):
+        return self.input_data[idx], self.output_data[idx]
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    numpy.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+# train / validation / test split
+tr_segments = segments_df[segments_df.aa != ''][segments_df.pretty][~segments_df.run.isin(chosen_runs)]#[segments_df.channel > 120]
+
+# Validation set used for testing architectures and hyperparameter tuning, but final model was trained with the entire training set
+# vl_segments = segments_df[segments_df.aa != ''][segments_df.pretty][~segments_df.run.isin(chosen_runs)][segments_df.channel <= 120]
+te_segments = segments_df[segments_df.aa != ''][segments_df.pretty][segments_df.run.isin(chosen_runs)]
+
+batch_size = 16
+
+
+    
+def get_data(segment_len, batch_size, acids=index_to_aa):
+    acid_to_index = {aa:i for i, aa in enumerate(acids)}
+
+    train_input = tr_segments[tr_segments.aa.isin(acids)].transformed.apply(lambda s: stretch(s, segment_len)).apply(torch.tensor).values
+#     val_input   = vl_segments[vl_segments.aa.isin(acids)].transformed.apply(lambda s: stretch(s, segment_len)).apply(torch.tensor).values
+    test_input  = te_segments[te_segments.aa.isin(acids)].transformed.apply(lambda s: stretch(s, segment_len)).apply(torch.tensor).values
+#     val_output    = vl_segments[vl_segments.aa.isin(acids)].aa.apply(lambda a: acid_to_index[a]).values
+    train_output = tr_segments[tr_segments.aa.isin(acids)].aa.apply(lambda a: acid_to_index[a]).values
+    test_output  = te_segments[te_segments.aa.isin(acids)].aa.apply(lambda a: acid_to_index[a]).values
+
+
+    # Create instances of the dataset and dataloader
+    train_dataset = MyDataset(train_input, train_output)
+#     val_dataset = MyDataset(val_input, val_output)
+    test_dataset = MyDataset(test_input, test_output)
+
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,worker_init_fn=seed_worker)
+#     vld_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True,worker_init_fn=seed_worker)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,worker_init_fn=seed_worker)
+
+    class_weights=class_weight.compute_class_weight(class_weight='balanced',classes=np.unique(train_output),y=train_output)
+    class_weights=torch.tensor(class_weights,dtype=torch.float)
+    return train_loader, test_loader, class_weights  #vld_loader,
+
+class CNN(nn.Module):
+    def __init__(self, hidden_size, dropout, use_gru, segment_len, init, 
+                 O_1=8, O_2=32, O_3=32, O_4=64, n_classes=20, 
+                 K_1 = 2, K_2 = 1, K_3 = 4, K_4 = 2,
+                 KP_1 = 4, KP_2 = 4, KP_3 = 1, KP_4 = 1,
+                 act='Th', n_layers=2):
+        
+        reshape = segment_len
+        self.act = act
+        self.conv_linear_out = int(math.floor((math.floor((math.floor((math.floor((math.floor((reshape - K_1 + 1)/KP_1) 
+                                                                       - K_2 + 1)/KP_2)
+                                                                       - K_3 + 1)/KP_3) 
+                                                                       - K_4 + 1)/KP_4))) * O_4)
+        self.FN_1 = hidden_size
+        self.use_gru = use_gru
+
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Sequential(nn.Conv1d(1, O_1, K_1), nn.ReLU(),
+                                   nn.MaxPool1d(KP_1))
+        self.conv2 = nn.Sequential(nn.Conv1d(O_1, O_2, K_2), nn.ReLU(),
+                                   nn.MaxPool1d(KP_2))
+        self.conv3 = nn.Sequential(nn.Conv1d(O_2, O_3, K_3), nn.ReLU(),
+                                   nn.MaxPool1d(KP_3))
+        self.conv4 = nn.Sequential(nn.Conv1d(O_3, O_4, K_4), nn.ReLU(),
+                                   nn.MaxPool1d(KP_4))
+        
+        self.gru = nn.GRU(input_size=self.conv_linear_out, hidden_size=self.FN_1, num_layers=n_layers, dropout=dropout)
+        self.fc1 = nn.Linear(self.conv_linear_out, self.FN_1, nn.Dropout(dropout))
+        
+        self.fc2 = nn.Linear(self.FN_1, n_classes)
+        
+        if init:
+            if init == "KN":
+                nn.init.kaiming_normal_(self.conv1[0].weight)
+                nn.init.kaiming_normal_(self.conv2[0].weight)
+                nn.init.kaiming_normal_(self.conv3[0].weight)
+                nn.init.kaiming_normal_(self.conv4[0].weight)
+                nn.init.kaiming_normal_(self.fc1.weight)
+                nn.init.kaiming_normal_(self.fc2.weight)
+            elif init == 'XN':
+                nn.init.xavier_normal_(self.conv1[0].weight)
+                nn.init.xavier_normal_(self.conv2[0].weight)
+                nn.init.xavier_normal_(self.conv3[0].weight)
+                nn.init.xavier_normal_(self.conv4[0].weight)
+                nn.init.xavier_normal_(self.fc1.weight)
+                nn.init.xavier_normal_(self.fc2.weight)
+            elif init == "KNC":
+                nn.init.kaiming_normal_(self.conv1[0].weight)
+                nn.init.kaiming_normal_(self.conv2[0].weight)
+                nn.init.kaiming_normal_(self.conv3[0].weight)
+                nn.init.kaiming_normal_(self.conv4[0].weight)
+                
+            elif init == 'XNC':
+                nn.init.xavier_normal_(self.conv1[0].weight)
+                nn.init.xavier_normal_(self.conv2[0].weight)
+                nn.init.xavier_normal_(self.conv3[0].weight)
+                nn.init.xavier_normal_(self.conv4[0].weight)
+
+    def forward(self, x):
+        x = x.float()
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2(x))
+        x = F.leaky_relu(self.conv3(x))
+        x = F.leaky_relu(self.conv4(x))
+        
+        x = x.view(len(x), -1)
+        if self.use_gru:
+            x = F.logsigmoid(self.gru(x)[0])
+        else:
+            x = F.logsigmoid(self.fc1(x)[0])
+            
+        x = self.fc2(x)
+        return x
+
+
+def train(model, optimizer,lmbd, epochs, criterion, train_loader): #, vld_loader
+    use_cuda = False
+    losses = []
+    accs = []
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        print("*", end='')
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            if use_cuda and torch.cuda.is_available():
+                inputs = inputs.cuda()
+                labels = labels.cuda()
+
+            optimizer.zero_grad()
+            outputs = model(inputs.unsqueeze(1))
+            loss = criterion(outputs, labels)
+
+            reg_loss = None
+            for param in model.parameters():
+                if reg_loss is None:
+                    reg_loss = 0.5 * torch.sum(param**2)
+                else:
+                    reg_loss = reg_loss + 0.5 * param.norm(2)**2
+
+            loss += lmbd * reg_loss
+
+            loss.backward()
+            optimizer.step()
+
+         # below is only used for hyperparmater tuning
+            
+#         correct = 0.0
+#         total = 0.0
+#         running_vloss = 0
+#         with torch.no_grad():
+#             for i, data in enumerate(vld_loader):
+#                 inputs, labels = data
+#                 if use_cuda and torch.cuda.is_available():
+#                     inputs = inputs.cuda()
+#                     labels = labels.cuda()
+
+#                 outputs = model(inputs.unsqueeze(1))
+#                 _, predicted = torch.max(outputs.data, 1)
+#                 running_vloss += criterion(outputs, labels).item()
+
+#                 correct += (predicted == labels).sum().item()
+#                 total += len(labels)
+#                 running_vloss += criterion(outputs, labels).item()
+                
+#         avg_vloss = running_vloss / (i + 1)
+#         losses.append(avg_vloss)
+#         accs.append(correct / total * 100.)
+#         if epoch %10 == 0:
+#             print(f"{correct / total * 100.}%")
+#     return losses, accs
+
+def top_n_cnt(y_test, test_pred, k):
+    cnt = 0
+    for yt, pred in zip(y_test, test_pred):
+        top_k = np.array(torch.topk(pred, k).indices)
+        if int(yt) in top_k:
+            cnt += 1
+    return cnt
+
+
+# if opt == 'SGD':
+batch_size = 16
+lmbd = 0
+segment_len = 100
+use_gru = True
+dropout = 0.3
+init = 'KN'
+hidden_size = 128
+momentum = 0.55
+epochs = 200
+use_weights = True
+use_gru = True
+act = 'Re'
+lr = 0.01
+n_layers = 2
+
+acids = index_to_aa
+train_loader, test_loader, class_weights = get_data(segment_len, batch_size, acids)   #vld_loader,
+
+top_n_experiment = np.zeros((20,20))
+for enum in range(20):
+
+    model = CNN(hidden_size, dropout, use_gru, segment_len, init, n_classes=len(acids), act=act, n_layers=n_layers)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum = momentum)
+    criterion = nn.CrossEntropyLoss(weight=class_weights) if use_weights else nn.CrossEntropyLoss()
+    train(model, optimizer, lmbd, epochs, criterion, train_loader) #losses, accs =  vld_loader
+    
+    with torch.no_grad():
+        for n_attempts in range(1,len(acids)+1):
+            if top_n_experiment[enum][n_attempts-1] != 0:
+                continue
+            total = 0
+            correct = 0
+            for data in test_loader:
+                inputs, labels = data
+                outputs = model(inputs.unsqueeze(1))
+                x, predicted = torch.max(outputs.data, 1)
+                total += len(labels)
+                correct += top_n_cnt(labels, outputs.data, n_attempts)
+            top_n_experiment[enum][n_attempts-1] = correct/total
+            
+    np.save(f"top_n_experiment.npy", top_n_experiment, allow_pickle=True)
+                        
---
+++ humpsClassifier.py
@@ -0,0 +1,141 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.utils import resample
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.decomposition import PCA
+import warnings, random
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+
+class RandomClassifier:
+    def __init__(self):
+       self.classes_ = None
+
+    def fit(self, X, y):
+        self.classes_ = np.unique(y)
+
+    def predict(self, X):
+        return random.choices(self.classes_, k=len(X))
+
+    def predict_proba(self, X):
+        rnums = [[random.random() for r in range(len(self.classes_))] for y in range(len(X))]
+        return [[r/sum(y_row) for r in y_row] for y_row in rnums]
+
+
+class HumpsClassifier:
+    def __init__(self):
+        self.pretty_segments_df = pd.read_pickle('data/pretty_segments_df.pkl')
+        self.channels_arr = list(np.load(open('data/channels_arr.npy','rb')))
+        self.feature_cols = ['median', 'max', 'middle', 'mean_abs_deriv', 'median_abs_deriv','mean','raw_std', 'dip']
+        index_to_aa = [c for c in 'CSAGTVNQMILYWFPHRKDE']
+        self.aa_to_index = {aa:i for i, aa in enumerate(index_to_aa)}
+
+    def create_rf_upsample_feature_matrix(self, acids, 
+                                    num_features=64, 
+                                    include_manual=True, 
+                                    include_matrix=True, 
+                                    accuracy_metric=accuracy_score,
+                                    n_repeats=5,
+                                    needs_classes = False,
+                                    test_runs = None,
+                                    classifiers = [
+                                        KNeighborsClassifier(3),
+                                        RandomForestClassifier(),
+                                        AdaBoostClassifier(), 
+                                        GaussianProcessClassifier()
+                                    ],
+                                    use_proba = False,
+                                    return_clf = False,
+                                    test_size = 0.2):
+
+        feature_cols = self.feature_cols 
+        aa_to_index = self.aa_to_index 
+
+        pretty_segments_df = self.pretty_segments_df
+        channels_arr = self.channels_arr
+
+        if not include_manual and not include_matrix:
+            print("You want me to predict without any features??")
+            return
+        accs = []
+        best_clfs = []
+        for i in range(n_repeats):
+            data_df = pretty_segments_df[pretty_segments_df.aa.isin(acids)]
+            if test_runs:
+                train_df = data_df[~data_df.run.isin(test_runs)]
+                test_df = data_df[data_df.run.isin(test_runs)]
+                assert len(set(train_df.index.values).intersection(set(test_df.index.values))) == 0
+            else: 
+                train_df, test_df = train_test_split(data_df, test_size=test_size)
+            del data_df
+
+            dfs = [train_df[train_df.aa == AA] for AA in acids]
+            lens = [len(x) for x in dfs]
+            biggest_aa_i = np.argmax(lens)
+            biggest_aa = acids[biggest_aa_i]
+            target_cnt = lens[biggest_aa_i]
+
+            # upsample minority residues in the TRAINING SET only
+            for i, AA_minority in enumerate(acids):
+                if AA_minority == biggest_aa:
+                    continue
+                assert target_cnt >= lens[i]
+                re_balance = target_cnt - lens[i]
+                df_minority_upsampled = resample(dfs[i], 
+                                                    replace=True,     # sample with replacement
+                                                    n_samples=re_balance,    # to match majority class
+                                                    random_state=42) # reproducible results
+                train_df = pd.concat([train_df, df_minority_upsampled])
+
+            # Convert amino acid letter to number for the classification values
+            y_train = [aa_to_index[a] for a in train_df.aa.values]
+            y_test = [aa_to_index[a] for a in test_df.aa.values]
+
+            # Extract the features from dataframe, based on arguments include_matrix and include_manual
+            if include_matrix:
+                pca_fit_df = pd.concat([train_df, pretty_segments_df[~pretty_segments_df.aa.isin(acids)]])
+                kosher_indices = [channels_arr.index(i) for i in pca_fit_df.index.values]
+                pca = PCA(num_features if num_features < len(pca_fit_df) else len(pca_fit_df))
+                pca.fit([x[kosher_indices] for i, x in enumerate(pca_fit_df.features.values) if i in kosher_indices])
+                
+                pca_X_train = pca.transform([x[kosher_indices] for x in train_df['features'].values])
+                pca_X_test = pca.transform([x[kosher_indices] for x in test_df['features'].values])
+
+                if include_manual:
+                    X_train = [np.concatenate((pca, x.to_numpy())) for pca, (_,x) in zip(pca_X_train, train_df.loc[:, feature_cols].iterrows())]
+                    X_test = [np.concatenate((pca, x.to_numpy())) for pca, (_,x) in zip(pca_X_test, test_df.loc[:, feature_cols].iterrows())]
+                else:
+                    X_train = pca_X_train
+                    X_test = pca_X_test
+
+            else: # only manual features 
+                X_train = train_df.loc[:, feature_cols]
+                X_test = test_df.loc[:, feature_cols]
+
+            if len(classifiers) == 1:
+                clf = classifiers[0]
+                clf.fit(X_train, y_train)  
+                test_pred = clf.predict_proba(X_test) if use_proba else clf.predict(X_test) 
+                accs.append(accuracy_metric(y_test, test_pred, clf.classes_) if needs_classes else accuracy_metric(y_test, test_pred))
+                best_clfs.append(clf)
+            else:
+                best_score = 0
+                best_classifier = 'None'
+                for clf in classifiers:
+                    clf.fit(X_train, y_train)
+                    test_pred = clf.predict_proba(X_test) if use_proba else clf.predict(X_test) 
+                    acc = accuracy_metric(y_test, test_pred, clf.classes_) if needs_classes else accuracy_metric(y_test, test_pred)
+                    if acc > best_score:
+                        best_score = acc
+                        best_classifier = clf.__class__
+                accs.append(best_score)
+                best_clfs.append(best_classifier)
+        # print(best_clfs)
+        # return accs
+        if return_clf:
+            return np.mean([a for a in accs if a >0]), accs, best_clfs
+        return np.mean([a for a in accs if a >0]), accs
